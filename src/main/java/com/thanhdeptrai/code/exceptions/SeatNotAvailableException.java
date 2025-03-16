3e5d1b12d9396a8bd333db083e732b08f11e92bd
Thanh Ro <thanhro@192.168.1.3>
2025-03-16 17:58:24 +0700

    add UT

--- src/main/java/com/thanhdeptrai/code/exceptions/SeatNotAvailableException.java
+++ src/main/java/com/thanhdeptrai/code/exceptions/SeatNotAvailableException.java
@@ -4,4 +4,8 @@ public class SeatNotAvailableException extends RuntimeException {
     public SeatNotAvailableException() {
         super("Seat is no longer available");
     }
+
+    public SeatNotAvailableException(String s) {
+        super(s);
+    }
 }
